3332a49f7060d9b2b97c14bf9eceefcbd026c6ee
JChauster <chaudown5@hotmail.com>
2016-02-06 19:51:40 -0500

    made dialogs but they no good

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 angular.module( 'Squire', [ 'ngMaterial' ] )
 
-.controller("AppCtrl", function($scope, $timeout, $mdSidenav, $log, $anchorScroll, $location) {
+.controller("AppCtrl", function($scope, $timeout, $mdSidenav, $log, $anchorScroll, $location, $mdDialog, $mdMedia) {
   // e.g. var a = new Command("test", ["blah", "blah"]);
   function VoiceCommand(name, commands) {
     this.name = name;
@@ -30,22 +30,14 @@ angular.module( 'Squire', [ 'ngMaterial' ] )
     var value = localStorage[key];
     console.log(key + " => " + value);
     $scope.voiceCommands.push({"name": key, "commands": value});
-  }
+  };
 
   $scope.deleteCommand = function (index) {
     var key = $scope.voiceCommands[index].name;
     $scope.voiceCommands.splice(index, 1);
     localStorage.removeItem(key);
-  }
-
-  $scope.range = function(min, max, step) {
-   step = step || 1;
-   var input = [];
-   for (var i = min; i <= max; i += step) {
-       input.push(i);
-   }
-   return input;
   };
+
 
   $scope.toggleLeft = buildDelayedToggler('left');
   $scope.toggleRight = buildToggler('right');
@@ -54,27 +46,78 @@ angular.module( 'Squire', [ 'ngMaterial' ] )
   };
   $scope.len = 0;
   $scope.customCommand = {
-    "custom-command" : "",
+    "custom_command" : "",
     "actions": [
-      {
-        "action" : "",
-        "args" : []
-      }
     ]
+  };
+
+   $scope.showAdvanced = function(ev) {
+    $scope.voiceCommands = [];
+    for(var i = 0, len=localStorage.length; i<len; ++i) {
+      var key = localStorage.key(i);
+      var value = localStorage[key];
+      console.log(key + " => " + value);
+      $scope.voiceCommands.push({"name": key, "commands": value});
+    };
+    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+    $mdDialog.show({
+      parent: angular.element(document.body),
+      templateUrl: 'command-dialog.tmpl.html',
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: useFullScreen,
+    })
+    $scope.$watch(function() {
+      return $mdMedia('xs') || $mdMedia('sm');
+    }, function(wantsFullScreen) {
+      $scope.customFullscreen = (wantsFullScreen === true);
+    });
+  };
+
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+
+  $scope.addAction = function(type) {
+    $scope.customCommand.actions.push({"action": type, "args": ""});
+  };
+
+  $scope.submitCommand = function() {
+    for (i = 0; i != $scope.customCommand.actions.length; ++i) {
+      $scope.customCommand.actions[i].args = document.getElementById('action_' + i).value;
+    }
+    var strJSON = JSON.stringify($scope.customCommand.actions);
+    var strCMD = document.getElementById('voice_command').value;
+    $scope.customCommand.custom_command = strJSON;
+    localStorage.setItem(strCMD, strJSON);
+    $scope.resetCommand();  
+    $scope.hide();
+    window.location.reload();
+  };
+
+  $scope.resetCommand = function() {
+    $scope.customCommand = {
+      "custom_command" : "",
+      "actions": [
+      ]
+    }
   }
-  /**
-   * Supplies a function that will continue to operate until the
-   * time is up.
-   */
-  $scope.newCommand = function() {
+  $scope.newCommand = function(cmd, json) {
     new Promise(function(resolve, reject) {
-        $scope.voiceCommands.push({"name": '', "commands": ''});
+        $scope.voiceCommands.push({"name": cmd, "commands": json});
         resolve();
       }).then(function() {
           $location.hash('bottom');
           $anchorScroll();             
       });
-  }
+  };
+
    function debounce(func, wait, context) {
     var timer;
     return function debounced() {
